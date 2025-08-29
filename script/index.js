6ec63571af749e5e9aff60f117c7556c5567e1b5
unknown <mijan90786@gmail.com>
2025-08-30 05:07:57 +0600

    add cart and price feature

--- script/index.js
+++ script/index.js
@@ -8,6 +8,8 @@ function getElement(id) {
 
 // // Event                   Delegation                          technique  
 
+const cartContainer = getElement("cart-container")
+
 getElement("product-box").addEventListener("click", function (e) {
     if (e.target.className.includes("cart-btn")) {
         const btn = e.target;
@@ -15,41 +17,60 @@ getElement("product-box").addEventListener("click", function (e) {
         const productImg = btn.parentNode.parentNode.parentNode.children[0].children[0].src
         const productTitle = btn.parentNode.parentNode.children[0].innerText
         const productPrice = btn.parentNode.parentNode.children[2].children[0].innerText
+        // console.log(productTitle)
 
 
-        const totalPrice = getElement("total-price").innerText
+        //         const totalPrice = getElement("total-price").innerText
+        //         let currentPrice = Number(productPrice) + Number(totalPrice)
+        // getElement("total-price").innerText = currentPrice.toFixed(2)
+
+        const cartHtmlCollection = document.getElementsByClassName("cart-product-title")
+        const arrayOfTitle = Array.from(cartHtmlCollection)
+        const isItemInCart = arrayOfTitle.find(item => item.innerText.trim() === productTitle);
+        // console.log(isItemInCart)
+        // console.log(isItemInCart)
+
+        if (isItemInCart) {
+            const currentItemDiv = isItemInCart.parentNode.parentNode
+            const quantitySpan = currentItemDiv.querySelector(".product-quantity")
+            const nowQuantity = parseInt(quantitySpan.innerText) + parseInt(1);
+            quantitySpan.innerText = nowQuantity;
+            const priceSpan = currentItemDiv.querySelector(".totalCostForOneItem")
+            const nowPrice = productPrice * nowQuantity
+            priceSpan.innerText = nowPrice.toFixed(2)
+            updateTotalPrice();
+            updateQuantity()
+        }
+        else {
+
+            const newCart = document.createElement("div")
+
+            newCart.innerHTML = `            <div class="flex justify-between rounded-xl  bg-gray-200 p-4">
+                          
+                                  <img src="${productImg}" alt="" class="w-10">
+      
+                          <div>
+                              <div> <button class="product-minus  "> - </button> <span class="product-quantity my-2"> 1 </span> <button class="product-add"> + </button></div>
+                              <div class="hidden"> <span > ${productPrice}</span> TK </div>
+                          </div>
+                      
+                              <div>
+                                  <h2 class="font-bold cart-product-title"> ${productTitle}</h2>
+                                  <h2 class="font-bold"> <span class="totalCostForOneItem">${productPrice}</span> TK</h2>
+                              </div>
+                             </div>`
 
 
-        let currentPrice = Number(productPrice) + Number(totalPrice)
+
+            cartContainer.append(newCart)
+            updateTotalPrice();
+            updateQuantity()
+        }
 
 
-        getElement("total-price").innerText = currentPrice.toFixed(2)
+        // const quantity = getElement("total-quantity").innerText;
 
-
-
-
-        const cartContainer = getElement("cart-container")
-        cartContainer.classList.add("block")
-
-        const newCart = document.createElement("div")
-
-        newCart.innerHTML = `  <div class="flex justify-between rounded-xl  bg-gray-200 p-4 ">
-                    
-                            <img src="${productImg}" alt="" class="w-10">
-                
-                        <div>
-                            <h2 class="font-bold"> ${productTitle}</h2>
-                            <h2 class="font-bold"> ${productPrice} TK</h2>
-                        </div>
-                       </div>`
-
-
-
-        cartContainer.append(newCart)
-
-        const quantity = getElement("total-quantity").innerText;
-
-        getElement("total-quantity").innerText = Number(quantity) + 1
+        // getElement("total-quantity").innerText = Number(quantity) + 1
 
 
 
@@ -69,4 +90,54 @@ getElement("product-box").addEventListener("click", function (e) {
 
 
     }
+
 })
+
+cartContainer.addEventListener("click", (e) => {
+    if (e.target.className.includes("product-minus")) {
+        const button = e.target;
+        const quantity = button.parentNode.children[1]
+        const price = button.parentNode.parentNode.children[1].children[0].innerText
+        const nowQuantity = Number(quantity.innerText) - Number(1);
+        if (nowQuantity < 0) {
+            return
+        }
+        quantity.innerText = nowQuantity;
+        const nowPrice = button.parentNode.parentNode.parentNode.children[2].children[1].children[0]
+        nowPrice.innerText = (price * nowQuantity).toFixed(2)
+        updateTotalPrice();
+        updateQuantity()
+
+    }
+    if (e.target.className.includes("product-add")) {
+        const button = e.target;
+        const quantity = button.parentNode.children[1]
+        const price = button.parentNode.parentNode.children[1].children[0].innerText
+        const nowQuantity = Number(quantity.innerText) + Number(1)
+        quantity.innerText = nowQuantity;
+        const nowPrice = button.parentNode.parentNode.parentNode.children[2].children[1].children[0]
+        nowPrice.innerText = (price * nowQuantity).toFixed(2)
+        updateTotalPrice();
+        updateQuantity()
+    }
+})
+
+
+
+function updateQuantity() {
+    const quantityElement = cartContainer.querySelectorAll(".product-quantity")
+    let totalQuantity = 0
+    quantityElement.forEach(el => {
+        totalQuantity += Number(el.innerText)
+    })
+    getElement("total-quantity").innerText = Number(totalQuantity)
+}
+
+function updateTotalPrice() {
+    const priceElement = cartContainer.querySelectorAll(".totalCostForOneItem")
+    let totalPrice = 0
+    priceElement.forEach(el => {
+        totalPrice += Number(el.innerText)
+    })
+    getElement("total-price").innerText = Number(totalPrice).toFixed(2)
+}
